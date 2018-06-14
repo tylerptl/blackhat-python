bce061ca3abde923bef6c178d4a6db33d1d3fbed
Tyler Crill <Tyler.W.Crill@hsutx.edu>
2018-06-14 14:45:49 -0500

    Fixed iteration issue in content_bruter

--- Chapter5/content_bruter.py
+++ Chapter5/content_bruter.py
@@ -1,7 +1,7 @@
 import urllib2, threading, Queue, urllib
 
-threads= 50
-target_url = "http://josephus.hsutx.edu"
+threads= 5
+target_url = "http://testphp.vulnweb.com"
 wordlist_file = "/tmp/all.txt" #SVNDigger
 resume = None
 user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0"
@@ -11,10 +11,12 @@ def build_wordlist(wordlist_file):
     fd = open(wordlist_file, "rb")
     raw_words = fd.readlines()
     fd.close()
+
     found_resume = False
     words = Queue.Queue()
 
     for word in raw_words:
+
         word = word.rstrip()
 
         if resume is not None:
@@ -27,3 +29,47 @@ def build_wordlist(wordlist_file):
         else:
             words.put(word)
     return words
+
+def dir_bruter(word_queue, extensions=None):
+    while not word_queue.empty():
+        attempt = word_queue.get()
+
+        attempt_list = []
+
+        #check for file extensions - if not its a dir path that is being bruted
+
+        if "." not in attempt:
+            attempt_list.append("/%s/" % attempt)
+        else:
+            attempt_list.append("/%s" % attempt)
+        #bruteforce extensions
+        if extensions:
+            for extension in extensions:
+                attempt_list.append("/%s%s" % (attempt, extension))
+
+        #iterate
+        for brute in attempt_list:
+
+            url = "%s%s" % (target_url,urllib.quote(brute))
+
+            try:
+                headers = {}
+                headers["User-Agent"] = user_agent
+
+                r = urllib2.Request(url, headers = headers)
+                response = urllib2.urlopen(r)
+
+                if len(response.read()):
+                    print "[%d] --> %s " % (response.code, url)
+            except urllib2.URLError, e:
+
+                if hasattr(e, 'code') and e.code != 404:
+                    print "*** %d --> %s" % (e.code, url)
+                    pass
+
+word_queue = build_wordlist(wordlist_file)
+extensions = [".php", ".bak", ".orig", ".inc"]
+
+for i in range(threads):
+    t = threading.Thread(target=dir_bruter, args = (word_queue,extensions))
+    t.start()
